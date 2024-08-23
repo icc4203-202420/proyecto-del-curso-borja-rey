9af72795a281bf5f8edcc4b1ca8790a4527f9a83
vicenterey <virey@miuandes.cl>
2024-08-23 15:53:00 -0400

    termine

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,15 @@
 class API::V1::UsersController < ApplicationController
   respond_to :json
-  before_action :set_user, only: [:show, :update]  
-  
+  before_action :authenticate_user!, only: [:create_friendship, :friendships]
+  before_action :set_user, only: [:show, :update, :friendships, :create_friendship]
+
   def index
-    @users = User.includes(:reviews, :address).all   
+    @users = User.includes(:reviews, :address).all
+    render json: @users, status: :ok
   end
 
   def show
-  
+    render json: @user, status: :ok
   end
 
   def create
@@ -20,11 +22,27 @@ class API::V1::UsersController < ApplicationController
   end
 
   def update
-    #byebug
     if @user.update(user_params)
       render :show, status: :ok, location: api_v1_users_path(@user)
     else
       render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def friendships
+    @friendships = @user.friendships.includes(:friend)
+    friends = @friendships.map(&:friend)
+    render json: friends, status: :ok
+  end
+
+  def create_friendship
+    friend = User.find(params[:friend_id])
+    @friendship = @user.friendships.build(friend: friend)
+
+    if @friendship.save
+      render json: @friendship, status: :ok
+    else
+      render json: @friendship.errors, status: :unprocessable_entity
     end
   end
 
@@ -35,11 +53,10 @@ class API::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.fetch(:user, {}).
-        permit(:id, :first_name, :last_name, :email, :age,
-            { address_attributes: [:id, :line1, :line2, :city, :country, :country_id, 
-              country_attributes: [:id, :name]],
-              reviews_attributes: [:id, :text, :rating, :beer_id, :_destroy]
-            })
+    params.fetch(:user, {}).permit(
+      :id, :first_name, :last_name, :email, :age,
+      address_attributes: [:id, :line1, :line2, :city, :country, :country_id, country_attributes: [:id, :name]],
+      reviews_attributes: [:id, :text, :rating, :beer_id, :_destroy]
+    )
   end
 end
