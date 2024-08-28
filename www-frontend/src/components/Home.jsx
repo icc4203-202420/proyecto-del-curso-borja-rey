e9c83c94264581c1cd8061ce539584177f15f060
BorjaManterola <bjmanterola@miuandes.cl>
2024-08-28 15:28:30 -0400

    Homepage sin homepage

---
+++ www-frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography, Button, Box } from '@mui/material';
+
+function Home() {
+  return (
+    <Container>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Homepage
+      </Typography>
+    </Container>
+  );
+}
+
+export default Home;  
