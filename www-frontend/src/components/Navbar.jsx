efc23d9f8ce51fb2dfd566d2aee074312b6c90b6
BorjaManterola <bjmanterola@miuandes.cl>
2024-09-10 13:46:49 -0300

    e 1.4

--- www-frontend/src/components/Navbar.jsx
+++ www-frontend/src/components/Navbar.jsx
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
